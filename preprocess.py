4f3f602fbdeadda0df1e473b1efe88a4378aeacf
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-06-29 12:44:39 -0700

    coding

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,18 @@
-haha
+
+import csv
+
+
+
+def preprocess():
+    filename = '/export/home/Dataset/EventCoref/gold_mention_in_cluster.tsv'
+
+
+    with open(filename) as tsvfile:
+        reader = csv.DictReader(tsvfile, dialect='excel-tab')
+        for row in reader:
+            print(row)
+            exit(0)
+
+
+if __name__ == "__main__":
+    preprocess()
