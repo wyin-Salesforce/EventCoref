675b63319e2c4734b405d1a894e8bcaa35d8fa58
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-06-30 16:51:28 -0700

    coding

--- event_entity_coref_ecb_plus-master/src/all_models/forfun.py
+++ event_entity_coref_ecb_plus-master/src/all_models/forfun.py
@@ -5,4 +5,4 @@ import pickle
 
 with open('/export/home/Dataset/EventCoref/processed/full_swirl_ecb/test_data', 'rb') as f:
     # test_data = cPickle.load(f)
-    test_data = pickle.loads(f)
+    test_data = pickle.load(f)
