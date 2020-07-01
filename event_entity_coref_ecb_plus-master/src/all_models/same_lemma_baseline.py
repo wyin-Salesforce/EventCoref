a5daa7e121e6ff17512854eefaaeffc98f3730d2
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-01 16:22:49 -0700

    coding

--- event_entity_coref_ecb_plus-master/src/all_models/same_lemma_baseline.py
+++ event_entity_coref_ecb_plus-master/src/all_models/same_lemma_baseline.py
@@ -159,7 +159,7 @@ def get_clusters_by_head_lemma_wenpeng(mentions, word2vec, is_event):
                     insert=True
                     break
                 else:
-                    comprehend_cos = max(lemma_cos, full_mention_cos)
+                    comprehend_cos = mean(lemma_cos, full_mention_cos)
                     '''add extra beyong lemma matching'''
                     if mention_i.gold_tag == mention_j.gold_tag:
                         diff_lemma_error+=1
