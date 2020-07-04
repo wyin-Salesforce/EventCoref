43d8403abad0bde42e4c5de5334ed5c655349d73
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-07-03 22:15:40 -0700

    coding

--- event_entity_coref_ecb_plus-master/src/all_models/same_lemma_baseline.py
+++ event_entity_coref_ecb_plus-master/src/all_models/same_lemma_baseline.py
@@ -174,7 +174,7 @@ def get_clusters_by_head_lemma_wenpeng(mentions, word2vec, is_event):
                     # if len(set(mention_i_arg1.split()+mention_i_arg2.split()) & set(mention_j_arg1.split()+mention_j_arg2.split())) > 0:
 
 
-                    if lemma_cos > 0.6 and (len(set(mention_i_arg1.split()+mention_i_arg2.split()) & set(mention_j_arg1.split()+mention_j_arg2.split())) > 0):
+                    if lemma_cos > 0.6:# and (len(set(mention_i_arg1.split()+mention_i_arg2.split()) & set(mention_j_arg1.split()+mention_j_arg2.split())) > 0):
                         # if comprehend_cos > 0.6:
                         if mention_i.gold_tag != mention_j.gold_tag:
                             diff_lemma_error_after+=1
