9d54b77901d444e34743237dfa26b552c441e47b
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-15 06:28:48 -0700

    coding

--- NLI/forfun.py
+++ NLI/forfun.py
@@ -1,6 +1,6 @@
 from transformers import RobertaTokenizer
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
-sent1 = tokenizer("Hello world")
-print(sent1, sent1['input_ids'])
-sent2 = tokenizer(" Hello world")
-print(sent2, sent2['input_ids'])
+sent1 = tokenizer.tokenize("Hello world")
+print(sent1)
+sent2 = tokenizer.tokenize(" Hello world")
+print(sent2)
