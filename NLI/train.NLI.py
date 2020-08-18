b4d12c0a9c5bc566b0a9c1258d2494f747ee8213
Wenpeng Yin <wyin@wyin-ltm.internal.salesforce.com>
2020-08-17 20:31:17 -0700

    coding

--- NLI/train.NLI.py
+++ NLI/train.NLI.py
@@ -748,7 +748,7 @@ def main():
                 iter_co+=1
                 # if iter_co %20==0:
                 # if iter_co % len(train_dataloader)==0:
-                if iter_co % (len(train_dataloader)/2)==0:
+                if iter_co % (len(train_dataloader)//2)==0:
                     '''
                     start evaluate on dev set after this epoch
                     '''
